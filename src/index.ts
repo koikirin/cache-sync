b6b69fc8de01a111e767230e224315eb243c2067
Hieuzest <girkirin@hotmail.com>
2023-07-23 22:16:04 +0800

    feat: synccache

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,71 @@
-import { Context, Schema } from 'koishi'
+import { Context, Service, Dict, Schema } from 'koishi'
 
-export const name = 'cache-sync'
-
-export interface Config {}
-
-export const Config: Schema<Config> = Schema.object({})
-
-export function apply(ctx: Context) {
-  // write your plugin here
+declare module 'koishi' {
+  interface Context {
+    synccache: MemoryCache
+  }
 }
+
+interface Entry {
+  value: any
+  timer?: NodeJS.Timer
+}
+
+class MemoryCache extends Service {
+  private store: Dict<Dict<Entry>> = Object.create(null)
+
+  constructor(ctx: Context, private config: MemoryCache.Config) {
+    super(ctx, 'synccache')  }
+
+  stop() {
+    for (const name in this.store) {
+      this.clear(name)
+    }
+  }
+
+  table(name: string): Dict<Entry> {
+    return this.store[name] ??= Object.create(null)
+  }
+
+  clear(name: string) {
+    const table = this.table(name)
+    for (const key in table) {
+      clearTimeout(table[key].timer)
+    }
+    delete this.store[name]
+  }
+
+  get(name: string, key: string) {
+    const table = this.table(name)
+    return table[key]?.value
+  }
+
+  set(name: string, key: string, value: any, maxAge?: number) {
+    this.delete(name, key)
+    const table = this.table(name)
+    table[key] = { value }
+    if (maxAge) {
+      table[key].timer = setTimeout(() => delete table[key], maxAge)
+    }
+  }
+
+  delete(name: string, key: string) {
+    const table = this.table(name)
+    if (table[key]) {
+      clearTimeout(table[key].timer)
+      delete table[key]
+    }
+  }
+
+  dump(name: string, remove?: boolean) {
+    
+  }
+}
+
+namespace MemoryCache {
+  export interface Config {}
+
+  export const Config: Schema<Config> = Schema.object({})
+}
+
+export default MemoryCache
